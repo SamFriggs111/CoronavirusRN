008c0eb12a3d89f045b2f9b00be79be199936e74
Sam Friggens <sam.friggens@adi.do>
2021-01-25 22:11:25 +0000

    added broken importer

---
+++ config.js
@@ -0,0 +1,12 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyABC3sm3wgjc2uG9kVpGoM9MQq-Q57TQt4",
+  authDomain: "nifty-inkwell-302821.firebaseapp.com",
+  databaseURL:
+    "https://coronavirusrn-65e9c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "nifty-inkwell-302821",
+  storageBucket: "nifty-inkwell-302821.appspot.com",
+  messagingSenderId: "335759551271"
+};
+// Exports
+module.exports = firebaseConfig;
